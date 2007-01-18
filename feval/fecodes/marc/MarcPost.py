6ee05647a5c985bc1b6b6ee1a402810b87822a86
tnoo <tnoo@7ec99d25-ca1a-0410-801a-b544497af6a6>
2007-01-18 20:06:11 +0000

    a MARC file reader that builds on py_post (in the MARC library)
    git-svn-id: svn://svn.berlios.de/feval/trunk@27 7ec99d25-ca1a-0410-801a-b544497af6a6

---
+++ feval/fecodes/marc/MarcPost.py
@@ -0,0 +1,65 @@
+# use py_post from MSC.MARC to access MARC Result files
+
+# set the path and import the MARC library
+import sys
+sys.path.append('../../../lib')
+import py_post as M
+import numpy as N
+import MarcShapeFunctions
+
+class MarcPostFile(object):
+    """
+    """
+
+    def __init__(self, model, filename):
+        self.model = model
+        self.filename = filename
+        try:
+            self.file = M.post_open(filename)
+        except:
+            print "could not open postfile %s" %filename
+            sys.exit(0)
+
+    def readInc(self, inc):
+        try:
+            self.file.moveto(inc)
+        except:
+            print "could not find increment %d" % inc
+
+        # read the nodes
+        for i in range(self.file.nodes()):
+            node = self.file.node(i)
+            self.model.setCoordinate(node.id, N.array([(node.x, node.y, node.z)]))
+
+        # read the elements
+        for i in range(self.file.elements()):
+            elem = self.file.element(i)
+            sh = MarcShapeFunctions.MarcShapeFunctionDict[elem.type]
+            if not sh[2] == elem.len:
+                print 'problem with number of nodes'
+            self.model.setElement(elem.id, sh[0], elem.items )
+
+        m.update()
+
+    def IncrementInfo(self):
+        """Plot information about increments"""
+        try:
+            return self.IncInfo
+        except:					
+            self.IncInfo = []
+            for inc in range(self.file.increments()-1):
+                self.file.moveto(inc+1)
+                self.IncInfo.append((self.file.increment, self.file.time))
+            return self.IncInfo
+
+if __name__ == '__main__':
+    from feval.FEval import *
+    
+    m  = ModelData()
+#     mf = MarcPostFile(m, '../../../data/marc/e7x1b.t16')
+#    mf = MarcPostFile(m, '/home/tinu/projects/gorner/model/marc-riesen/gorner_enhsl.t16')
+    mf = MarcPostFile(m, '/home/tinu/projects/colle/marc/ca8_t.t16')
+    mf.readInc(1)
+
+    print 'test finished'
+    
