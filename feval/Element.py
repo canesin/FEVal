265590d2c012cbea0385725d92f4754c64d30d20
tnoo <tnoo@7ec99d25-ca1a-0410-801a-b544497af6a6>
2007-02-14 07:40:05 +0000

    added the calculation of spatial derivative of nodvar
    git-svn-id: svn://svn.berlios.de/feval/trunk@42 7ec99d25-ca1a-0410-801a-b544497af6a6

--- feval/Element.py
+++ feval/Element.py
@@ -19,47 +19,59 @@
 #============================================================
 
 import numpy as N
+import numpy.linalg as LA
+import scipy.optimize
 
 #============================================================================
 # try to import Scientific Python (scipy.sourceforge.net)
 
-try:
-    import scipy.optimize._minpack
-    print 'using Scipy'
+# try:
+#     import scipy.optimize._minpack
+#     print 'using Scipy directly'
 
-    def minimize(element, globalcoord, accuracy=1.49012e-8):
-        """Find the local coordinates in the system of |element| if
-        the global coordinates |globalcoord| are given.
-        This is achieved with a Newton
-        procedure. The initial local coordinate is [0, 0].
-        The algorithm is evaluated until the accuracy of the result is
-        better than |accuracy| (in local coordinates).
+#     def minimize(element, globalcoord, accuracy=1.49012e-8):
+#         """Find the local coordinates in the system of |element| if
+#         the global coordinates |globalcoord| are given.
+#         This is achieved with a Newton
+#         procedure. The initial local coordinate is [0, 0].
+#         The algorithm is evaluated until the accuracy of the result is
+#         better than |accuracy| (in local coordinates).
 
-        We do this call the function directly, without the error checking in
-        scipy.optimize.fsolve
-        """
-        retval = scipy.optimize._minpack._hybrd(element.calcDev, \
-                                                (0,)*element.dim,\
-                                                (globalcoord,), 0, \
-                                                accuracy, 200, -10, 10,\
-                                                0.0, 100, None)
+#         We do this call the function directly, without the error checking in
+#         scipy.optimize.fsolve
+#         """
+#         retval = scipy.optimize._minpack._hybrd(element.calcDev, \
+#                                                 (0,)*element.dim,\
+#                                                 (globalcoord,), 0, \
+#                                                 accuracy, 200, -10, 10,\
+#                                                 0.0, 100, None)
+#         return retval[0]
 
+#     print 'using Scipy normally'
 #         # somewhat slower version with error checking
+#     def minimize(element, globalcoord, accuracy=1.49012e-8):
 #         retval = scipy.optimize.fsolve(element.calcDev, element.lcoord,
 #                                        full_output=1, \
 #                                        #fprime = element.calcDDev, \
 #                                        args=(globalcoord,), xtol = accuracy)
 
-        return retval[0]
+#         return retval[0]
 
 
-except ImportError:
-    print 'Scipy not found'
-    raise ImportError
+# except ImportError:
+#     print 'Scipy not found'
+#     raise ImportError
 
-except:
-    print 'problems defining scipy shortcut'
-    raise ImportError
+# except:
+#     print 'problems defining scipy shortcut'
+#     raise ImportError
+
+def minimize(element, globalcoord, accuracy=1.49012e-8):
+    retval = scipy.optimize.fsolve(element.calcDev, element.lcoord,
+                                       full_output=1, \
+                                       #fprime = element.calcDDev, \
+                                       args=(globalcoord,), xtol = accuracy)
+    return retval[0]
 
 
 class Element:
@@ -68,9 +80,9 @@ class Element:
     o dim and nnodes: number of (space) dimensions and number of nodes 
     o nodes is a list of node names (not instances, just the keys for the
 	  model nodes dictionary)
-    o name is the name of the element (the key of the element dictionary)
+    o name is the name of the element (the key in the model element dictionary)
     o nodcoord: array to keep the nodal coordinates
-    o lcoord is to keep the actual local coordinates
+    o lcoord is to keep the current local coordinates
     """
 
     def __init__(self, nodes, shape, nodcoord, name = None):
@@ -81,25 +93,40 @@ class Element:
         # cut the list of the nodes to the actual nodes
         # (e.g. MARC sometimes has internal nodes)
         self.nodes    = nodes[:self.nnodes]
-        self.nodcoord = nodcoord[:self.nnodes]
+        self.nodcoord = nodcoord[:self.nnodes,:self.shape.dim]
         self.N_ik = None
 
-    def mapNodalVar(self, nodalvar, lcoord=None):
+    def __repr__(self):
+        return "Element %s" % str(self.name)
+
+    def __str__(self):
+        return "Element %s" % str(self.name)
+
+    def mapNodalVar(self, nodalvar, lcoord=None, deriv=False):
         """Interpolate the nodal variables to the point given in
-        |lcoord| using the element shape function"""
+        |lcoord| using the element shape function
+        if |deriv| is True calculate the spatial derivative of nodalvar"""
         if lcoord != None:
             self.lcoord = lcoord
-        sh = self.shape.calcShape(self.lcoord) # the shape functions
-        return N.sum(nodalvar * sh[N.newaxis,:], 1)
+        if deriv:
+            sh = self.shape.calcShapeDeriv(self.lcoord) # the shape function derivatives
+            inverse_jacobi = LA.inv(N.dot(self.nodcoord.transpose(), sh.transpose()))
+            return N.dot(nodalvar, sh.transpose())*inverse_jacobi
+        else:
+            sh = self.shape.calcShape(self.lcoord) # the shape functions
+            return N.dot(nodalvar, sh.transpose())
 
-    def mapIntPointVar(self, intpointvar, lcoord=None):
+    def mapIntPointVar(self, intpointvar, lcoord=None, deriv=False):
         """Interpolate the interpolation point variables to the point given in
         |lcoord| using the element shape function"""
         if lcoord != None:
             self.lcoord = lcoord
         if self.shape.gaussShapeInv == None:
             self.shape.calcGauss()
-        sh = self.shape.calcShape(self.lcoord) # the shape functions
+        if deriv:
+            sh = self.shape.calcShapeDeriv(self.lcoord) # the shape function derivatives
+        else:
+            sh = self.shape.calcShape(self.lcoord) # the shape functions
         nodvars = N.dot(self.shape.gaussShapeInv, intpointvar)
         return N.dot( sh, nodvars )
 
@@ -124,32 +151,36 @@ class Element:
         better than |accuracy| (in local coordinates)."""
 
         # We use the pluggable function "minimize".
-        self.lcoord = minimize( self, globalcoord, accuracy )
+        self.lcoord = minimize( self, globalcoord[:self.shape.dim], accuracy )
         return self.lcoord
 
     def containsPoint(self, globalcoord, accuracy = 1e-5):
         """Test whether the element contains a point given in global
         coordinates
         """
-        lcoord = minimize( self, globalcoord, accuracy )
+        lcoord = minimize( self, globalcoord[:self.shape.dim], accuracy )
         return (N.absolute(lcoord) <= 1.+accuracy).all()
 
     def setLocalCoord(self, lcoord):
         """set the local coorinate to |lcoord|"""
         self.lcoord = lcoord
 
-
 if __name__ == '__main__':
     
     import ShapeFunctions
     sh = ShapeFunctions.shapeFunctions['Quad4']()
     nodes = [1, 2, 3, 4]
-    nodcoord = N.array(([0,0], [2,0], [3,2], [0,2]),dtype=N.float_)
+    #nodcoord = N.array(([0,0], [2,0], [3,2], [0,2]),dtype=N.float_)
+    nodcoord = N.array(([0.,0.], [1.,0.], [1,2], [0,2.]),dtype=N.float_)
     e = Element( nodes, sh, nodcoord, 1 )
 
+    print e.findLocalCoord([1., 1.])
     print e.findGlobalCoord(e.findLocalCoord([10., 21.]))
     print e.findLocalCoord(e.findGlobalCoord([0., -1.]))
     print e.containsPoint([0., 2.], accuracy=0.01)
 
-    print e.findGlobalCoord([0., 1.])
+    print e.findGlobalCoord([-2., 0.])
+
+    nodvar = e.nodcoord.transpose()
     
+    print e.mapNodalVar(nodvar,deriv=True)
