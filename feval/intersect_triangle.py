a0a99a53a3a97e95862a743e9eb636a8bcd6612e
tnoo <tnoo@7ec99d25-ca1a-0410-801a-b544497af6a6>
2007-06-22 13:14:23 +0000

    *** empty log message ***
    git-svn-id: svn://svn.berlios.de/feval/trunk@53 7ec99d25-ca1a-0410-801a-b544497af6a6

--- feval/intersect_triangle.py
+++ feval/intersect_triangle.py
@@ -1,3 +1,5 @@
+# -*- encoding:iso-8859-1 -*-
+#
 # fast intersection of a ray with a triangle
 # code from http://jgt.akpeters.com/papers/MollerTrumbore97/code.html
 # adapted to numpy
@@ -16,42 +18,72 @@
 import numpy as N
 EPSILON = 0.000001
 
-def intersect_triangle(orig, dir, vert0, vert1, vert2):
+def intersect_triangle(orig, direction, vertices, culling=True):
 
+    direction = -direction
+    
     # find vectors for two edges sharing vert0
-    edge1= vert1- vert0
-    edge2= vert2- vert0
+    edge1= vertices[1] - vertices[0]
+    edge2= vertices[2] - vertices[0]
 
     # begin calculating determinant - also used to calculate U parameter
-    pvec = N.cross(dir, edge2)
+    pvec = N.cross(direction, edge2)
 
     # if determinant is near zero, ray lies in plane of triangle 
     det = N.dot(edge1, pvec)
 
-    if (det > -EPSILON and det < EPSILON):
-        return None
-    inv_det = 1.0 / det
+    if culling:
+        if (det < EPSILON):
+            return None
 
-    # calculate distance from vert0 to ray origin 
-    tvec = orig - vert0
+        # calculate distance from vert0 to ray origin */
+        tvec = orig - vertices[0]
+        
+        # calculate U parameter and test bounds 
+        u = N.dot(tvec, pvec)
+        if (u < 0.0 or u > det):
+            return None
 
-    # calculate U parameter and test bounds
-    u = N.dot(tvec, pvec) * inv_det
-    if (u < 0.0 or u > 1.0):
-        return None
+        # prepare to test V parameter 
+        qvec = N.cross(tvec, edge1)
 
-    # prepare to test V parameter
-    qvec = N.cross(tvec, edge1)
+        # calculate V parameter and test bounds 
+        v = N.dot(direction, qvec)
+        if (v < 0.0 or u + v > det):
+            return None
 
-    # calculate V parameter and test bounds
-    v = N.dot(dir, qvec) * inv_det
-    if (v < 0.0 or u + v > 1.0):
-        return None
+        # calculate t, scale parameters, ray intersects triangle 
+        t = N.dot(edge2, qvec)
+        inv_det = 1.0 / det
+        t *= inv_det
+        u *= inv_det
+        v *= inv_det
+        
+    else:
+        if (det > -EPSILON and det < EPSILON):
+            return None
+        inv_det = 1.0 / det
 
-    # calculate t, ray intersects triangle
-    t = N.dot(edge2, qvec) * inv_det
+        # calculate distance from vert0 to ray origin 
+        tvec = orig - vertices[0]
 
-    return t, u, v
+        # calculate U parameter and test bounds
+        u = N.dot(tvec, pvec) * inv_det
+        if (u < 0.0 or u > 1.0):
+            return None
+
+        # prepare to test V parameter
+        qvec = N.cross(tvec, edge1)
+
+        # calculate V parameter and test bounds
+        v = N.dot(direction, qvec) * inv_det
+        if (v < 0.0 or u + v > 1.0):
+            return None
+
+        # calculate t, ray intersects triangle
+        t = N.dot(edge2, qvec) * inv_det
+
+    return t, u, v, vertices[0] + u*edge1 + v*edge2
 
 if __name__ == "__main__":
 
@@ -61,4 +93,5 @@ if __name__ == "__main__":
 
     x0 = N.array((4, 3.5, 0))
     
-    print intersect_triangle(N.zeros(3), N.array([3.5, 5, 0]), x1, x2, x3)
+    print intersect_triangle(N.zeros(3), N.array([3.5, 5, 0]), [x1, x2, x3])
+    print intersect_triangle(N.zeros(3), N.array([3.5, 5, 0]), [x1, x2, x3], culling=False)
