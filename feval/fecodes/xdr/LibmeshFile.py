bd7b551f288f8aa884d2bb210bc5d480cd768111
tnoo <tnoo@7ec99d25-ca1a-0410-801a-b544497af6a6>
2007-08-08 07:58:54 +0000

    write binary data
    git-svn-id: svn://svn.berlios.de/feval/trunk@59 7ec99d25-ca1a-0410-801a-b544497af6a6

--- feval/fecodes/xdr/LibmeshFile.py
+++ feval/fecodes/xdr/LibmeshFile.py
@@ -41,7 +41,64 @@ class LibmeshFile(object):
         else:
             print "don't know what to do"
 
+    def writeFile(self, filename, bcond=None):
+        """write either an ASCII or binary file 
+        """
+        if filename.endswith('.xda'):
+            self.writeFileASCII(filename, bcond=bcond)
+        elif filename.endswith('.xdr'):
+            self.writeFileBinary(filename, bcond=bcond)
+        else:
+            print "don't know what to do"
+
     def readFileBinary(self, filename):
+        """read the binary file
+        """
+        import struct
+        f = file(filename,'rb')
+
+        bytes = f.read(12)
+        filetype, level = bytes[4:8], bytes[9]
+        if not filetype == 'LIBM':
+            print 'file type not (yet) supported', filetype
+            stop
+
+        nelems, nnodes, sumweight, nbcond, x, neleblocks = struct.unpack('!6i', f.read(24))
+        eletypes = struct.unpack('!%si'%neleblocks, f.read(4*neleblocks))
+        elenums  = struct.unpack('!%si'%neleblocks, f.read(4*neleblocks))
+        # read the titles
+        f.read(32)
+
+        # read the element block
+        conn = N.array(struct.unpack('!%si'%sumweight, f.read(4*sumweight)))
+        import feval.ShapeFunctions
+        cnt = 0
+        for elem, nelem in zip(eletypes, elenums):
+            if not self.shapeFunctionDict.has_key(str(elem)):
+                print 'No shape function found for element type', elem
+            elemtype = self.shapeFunctionDict[str(elem)]
+            shape = feval.ShapeFunctions.shapeFunctions[elemtype]
+            blocklen = nelem*(shape.nnodes+2)
+            print blocklen, nelem
+            for nodes in conn[cnt:cnt+blocklen].reshape(nelem, blocklen//nelem):
+                self.model.setElement(nodes[-2], elemtype, nodes[:-2] )
+
+            cnt += blocklen
+
+        # read the node block
+        coord = N.array(struct.unpack('!%sd'%(3*nnodes), f.read(8*3*nnodes)))
+        coord.shape = ( len(coord)//3, 3)
+        for n, c in enumerate(coord):
+            self.model.setCoordinate(n, c)
+
+        # read the boundary conditions
+        if nbcond > 0:
+            bcond = N.array(struct.unpack('!%si'%(3*nbcond), f.read(4*3*nnodes)))
+            bcond.shape = (nbcond, 3)
+            self.bcond = bcond
+        f.close()
+
+    def readFileBinary2(self, filename):
         """read the binary file
         """
         bytes = file(filename,'rb').read()
@@ -86,6 +143,7 @@ class LibmeshFile(object):
             bcond = N.fromstring(bytes[ptr:ptr+4*3*nbcond], '>i')
             bcond.shape = (nbcond, 3)
             self.bcond = bcond
+
 
     def readFileASCII(self, filename):
         """Parse an ASCII input file
@@ -146,7 +204,9 @@ class LibmeshFile(object):
                 bcond.append(map(int, lines[cnt+n].split()))
             self.bcond = N.array(bcond)
 
-    def writeFile(self, filename, bcond = None):
+
+
+    def writeFileASCII(self, filename, bcond = None):
         """Write a Libmesh xda file
         |bcond| should be a list of lists
         [[e1, s1, b1],
@@ -173,10 +233,10 @@ class LibmeshFile(object):
         lines.append('%d\t # Num. Elements\n' % len(self.model.Conn) )
         lines.append('%d\t # Num. Nodes\n'    % len(self.model.Coord) )
         lines.append('%d\t # Sum of Element Weights\n' % weight )
-        n_bound = 0
-        if bcond:
-            n_bound = len( bcond )
-        lines.append('%d\t # Num. Boundary Conds.\n' % n_bound)
+        nbcond = 0
+        if bcond != None:
+            nbcond = len( bcond )
+        lines.append('%d\t # Num. Boundary Conds.\n' % nbcond)
         lines.append('65536\t # String Size (ignore)\n') 
         lines.append('%d\t # Num. Element Blocks.\n' % len(elines.keys()) )
         etypes, eblocks = '', ''
@@ -199,16 +259,80 @@ class LibmeshFile(object):
 
         # write all coordinates
         ckeys = sorted(self.model.getCoordinateNames())
-        ndir = len(self.model.Coord[ckeys[0]])
-        for id in ckeys:
-            lines.append('%f '*ndir % tuple(self.model.getCoordinate(id)) + '\n')
+        for k in ckeys:
+            lines.append('%f %f %f \n' % tuple(self.model.getCoordinate(k)))
 
-        if bcond:
+        if bcond != None:
             lines.append('\n')
             for bc in bcond:
                 lines.append('%d %d %d\n' % tuple(bc))
 
         file(filename,'w').writelines(lines)
+        
+
+    def writeFileBinary(self, filename, bcond=None):
+        """write the binary file
+        """
+        # go through all elements and see which ones have the same type
+        # build a dict of elements
+        weight = 0 # total weight
+        etypes = {}
+        for ele in self.model.getElementNames():
+            etype, conn = self.model.getElementConn(ele)
+            nnod = len(conn)
+            etypes.setdefault(etype, []).append(ele)
+            weight += nnod
+        weight += 2*len(self.model.Conn)
+
+        nbcond = 0
+        if bcond != None:
+            nbcond = len( bcond )
+
+        # write the data
+        import struct
+        f = file(filename,'wb')
+
+        f.write(struct.pack('!L', 6))
+        f.write('LIBM 0\x00\x00')
+        f.write(struct.pack('!6L',
+                            len(self.model.Conn),
+                            len(self.model.Coord),
+                            weight,
+                            nbcond,
+                            65536,
+                            len(etypes),
+                            ))
+        ekeys = sorted(etypes.keys())
+        for k in ekeys:
+            f.write(struct.pack('!L', int(self.invShapeFunctionDict[k])))
+        for k in ekeys:
+            f.write(struct.pack('!L', len(etypes[k])))
+        s = 'Id String'
+        f.write(struct.pack('!L',len(s)))
+        f.write(struct.pack('!12s', s))
+        s = 'Title String'
+        f.write(struct.pack('!L',len(s)))
+        f.write(struct.pack('!12s', s))
+
+        # write the element block
+        for k in ekeys:
+            for ele in etypes[k]:
+                etype, conn = self.model.getElementConn(ele)
+                f.write(struct.pack('!%sL' %len(conn), * conn))
+                f.write(struct.pack('!L', ele))
+                f.write(struct.pack('!L', -1))
+        
+        # write the coordinate block
+        ckeys = sorted(self.model.getCoordinateNames())
+        for k in ckeys:
+            f.write(struct.pack('!3d', * self.model.getCoordinate(k)))
+
+        if bcond != None:
+            for bc in bcond:
+                f.write(struct.pack('!3L', * bc))
+
+        f.close()
+
         
 
 ### Test
@@ -218,24 +342,19 @@ if __name__ == '__main__':
 
     m = feval.FEval.FEModel()
     mf = LibmeshFile(m)
-    #mf.readFile('/soft/numeric/libmesh/examples/ex10/mesh.xda')
-    #mf.readFile("/soft/numeric/libmesh/reference_elements/3D/one_hex20.xda");
-    #     bc = [[0,1, 77],
-    #           [3,2, 1000]]
+#     mf.readFile('/soft/numeric/libmesh/examples/ex10/mesh.xda')
+#     #mf.readFile("/soft/numeric/libmesh/reference_elements/3D/one_hex20.xda");
+#     bc = [[0,1, 77],
+#           [3,2, 1000]]
 
-    #     mf.writeFile('/home/tinu/projects/libmesh/ex2/a2.xda', bcond = bc)
+#     mf.writeFile('/home/tinu/projects/libmesh/ex2/a2.xda', bcond = bc)
 
     mf.readFile('/home/tinu/projects/libmesh/ex2/a2.xda')
+    mf.writeFile('/home/tinu/projects/libmesh/ex2/a3.xda', bcond = mf.bcond)
+    #mf.writeFile('/home/tinu/projects/libmesh/ex2/a3.xdr')
+    mf.writeFile('/home/tinu/projects/libmesh/ex2/a3.xdr', bcond = mf.bcond)
 
-    m2 = feval.FEval.FEModel()
-    mf2 = LibmeshFile(m2)
-    mf2.readFile('/home/tinu/projects/libmesh/ex2/a2.xdr')
-
-
-
-
-
-
-
-
+#     m2 = feval.FEval.FEModel()
+#     mf2 = LibmeshFile(m2)
+#     mf2.readFile('/home/tinu/projects/libmesh/ex2/a2.xdr')
 
